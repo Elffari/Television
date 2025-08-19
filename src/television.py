3d1d778958367238e09494f93b84b41bf141bdd2
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2025-08-20 00:04:10 +0300

    Default channel to Lumiukko

--- src/television.py
+++ src/television.py
@@ -6,7 +6,7 @@ import time
 class Television:
     def __init__(self):
         self.volume = 100
-        self.current_channel = "1" # Default channel index
+        self.current_channel = "24" # Default channel index
         self.is_on = False # Track power state
         self.player = None
 
